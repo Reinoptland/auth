8f962a31077a8d3368f73115e2572c37af40c5e1
Reinoptland <reinoptland@gmail.com>
2022-09-26 15:23:24 +0200

    Persist new users, hash passwords

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,4 +1,8 @@
 import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
 
 const User = z.object({
   email: z.string().email(),
@@ -8,11 +12,49 @@ const User = z.object({
   phone: z.string(),
 });
 
-export default function handler(req, res) {
+function isRequestValidationError(error) {
+  return error.hasOwnProperty("issues");
+}
+
+function isDatabaseError(error) {
+  return error.hasOwnProperty("code");
+}
+
+function hashPassword(myPlaintextPassword) {
+  console.log("SALT", process.env.SALT_ROUNDS);
+  return bcrypt.hashSync(myPlaintextPassword, Number(process.env.SALT_ROUNDS));
+}
+
+export default async function handler(req, res) {
   try {
-    const user = User.parse(req.body);
-    res.status(200).json({ name: "John Doe" });
+    const validatedUser = User.parse(req.body);
+    const savedUser = await prisma.user.create({
+      data: {
+        ...validatedUser,
+        password: hashPassword(validatedUser.password),
+      },
+    });
+    res.status(200).json(savedUser);
   } catch (error) {
-    res.status(400).json({ errors: error.issues });
+    if (isRequestValidationError(error)) {
+      return res.status(400).json({
+        message: "Some fields in the form were not filled in correctly",
+        errors: error.issues,
+      });
+    } else if (isDatabaseError(error)) {
+      switch (error.code) {
+        case "P2002":
+          return res
+            .status(400)
+            .json({ message: "A user with this email already exists" });
+
+        default:
+          return res.status(400).json({
+            message: "Something unexpted happened please contact support",
+          });
+      }
+    } else {
+      return res.status(500).json({ message: "Oh no, something unexpected!" });
+    }
   }
 }

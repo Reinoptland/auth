f2d079ee224d979c3ffd71821ad66d5596268387
Reinoptland <reinoptland@gmail.com>
2022-09-26 15:30:18 +0200

    Slpit up /api/users into additional functions

--- pages/api/users.js
+++ pages/api/users.js
@@ -12,20 +12,18 @@ const User = z.object({
   phone: z.string(),
 });
 
-function isRequestValidationError(error) {
-  return error.hasOwnProperty("issues");
-}
-
-function isDatabaseError(error) {
-  return error.hasOwnProperty("code");
-}
-
-function hashPassword(myPlaintextPassword) {
-  console.log("SALT", process.env.SALT_ROUNDS);
-  return bcrypt.hashSync(myPlaintextPassword, Number(process.env.SALT_ROUNDS));
-}
-
 export default async function handler(req, res) {
+  switch (req.method) {
+    case "POST":
+      return create(req, res);
+
+    default:
+      return res.status(404).json({ message: "This route does not exist" });
+  }
+}
+
+async function create(req, res) {
+  console.log(req.method);
   try {
     const validatedUser = User.parse(req.body);
     const savedUser = await prisma.user.create({
@@ -34,27 +32,52 @@ export default async function handler(req, res) {
         password: hashPassword(validatedUser.password),
       },
     });
-    res.status(200).json(savedUser);
+    return res.status(200).json(savedUser);
   } catch (error) {
     if (isRequestValidationError(error)) {
-      return res.status(400).json({
-        message: "Some fields in the form were not filled in correctly",
-        errors: error.issues,
-      });
+      return handleValidationError(res, error);
     } else if (isDatabaseError(error)) {
-      switch (error.code) {
-        case "P2002":
-          return res
-            .status(400)
-            .json({ message: "A user with this email already exists" });
-
-        default:
-          return res.status(400).json({
-            message: "Something unexpted happened please contact support",
-          });
-      }
+      return handleDatabaseError(error, res);
     } else {
-      return res.status(500).json({ message: "Oh no, something unexpected!" });
+      return handleUnknownError(res);
     }
   }
+}
+
+function hashPassword(myPlaintextPassword) {
+  console.log("SALT", process.env.SALT_ROUNDS);
+  return bcrypt.hashSync(myPlaintextPassword, Number(process.env.SALT_ROUNDS));
+}
+
+function isRequestValidationError(error) {
+  return error.hasOwnProperty("issues");
+}
+
+function isDatabaseError(error) {
+  return error.hasOwnProperty("code");
+}
+
+function handleUnknownError(res) {
+  return res.status(500).json({ message: "Oh no, something unexpected!" });
+}
+
+function handleDatabaseError(error, res) {
+  switch (error.code) {
+    case "P2002":
+      return res
+        .status(400)
+        .json({ message: "A user with this email already exists" });
+
+    default:
+      return res.status(400).json({
+        message: "Something unexpted happened please contact support",
+      });
+  }
+}
+
+function handleValidationError(res, error) {
+  return res.status(400).json({
+    message: "Some fields in the form were not filled in correctly",
+    errors: error.issues,
+  });
 }
